9baa58764281d7e4c861fa863bdefe354109d9c9
qwertovv <muhiniljusha@yandex.ru>
2026-01-22 11:27:28 +0300

    Настройка заголовков и параметров запросов

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import requests
 import os
 
 class ImageDownloader:
@@ -9,6 +9,11 @@ class ImageDownloader:
         
         self.classes = ['tiger', 'leopard']
         self.class_folders = {}
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+        }
     
     def create_class_folders(self):
         for class_name in self.classes:
